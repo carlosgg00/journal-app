6cf48d89e458f692e8694ab7c403f4c11ebfbce5
carlosgg00 <garciaariel@unah.hn>
2023-02-05 15:27:20 -0600

    recargar pagina arreglado

--- src/JournarlApp.jsx
+++ src/JournarlApp.jsx
@@ -1,10 +1,13 @@
+import { HashRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
 import { AppTheme } from "./theme"
 
 export const JournarlApp = () => {
   return (
     <AppTheme>
+      <HashRouter>
         <AppRouter />
+      </HashRouter>
     </ AppTheme>
   )
 }
