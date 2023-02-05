45820024de8a124583421f8136deffab9c58f4cd
carlosgg00 <garciaariel@unah.hn>
2023-02-05 15:08:04 -0600

    login with google arreglado

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -10,17 +10,15 @@ import { useForm } from "../../hooks/useForm"
 import { startGoogleSignIn, startLoginWithEmailPassword } from "../../store/auth/"
 
 
-const formData = {
-  email: '',
-  password: ''
-}
-
 export const LoginPage = () => {
 
   const { status, errorMessage } = useSelector( state => state.auth );
 
   const dispatch = useDispatch();
-  const { email, password, onInputChange } = useForm( formData );
+  const { email, password, onInputChange } = useForm({
+    email: '',
+    password: ''
+  });
 
   const isAuthenticating = useMemo(() => status === 'checking', [status])
 
@@ -36,85 +34,81 @@ export const LoginPage = () => {
   }
 
 
-  return (
+ return (
     <AuthLayout title="Login">
-      <form onSubmit={ onSubmit } className="animate__animated animate__fadeIn animate__faster">
+      <form onSubmit={ onSubmit } className='animate__animated animate__fadeIn animate__faster'>
+          <Grid container>
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder='correo@google.com' 
+                fullWidth
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+              />
+            </Grid>
 
-      <Grid container>
-        <Grid item xs={ 12 } sx={{ mt: 2}}>
-          <TextField
-            label="Correo"
-            type="email"
-            placeholder="correo@google.com"
-            fullWidth
-            name= "email"
-            value={ email }
-            onChange={ onInputChange }
-          />
-        </Grid>
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder='Contraseña' 
+                fullWidth
+                name="password"
+                value={ password }
+                onChange={ onInputChange }
+              />
+            </Grid>
 
-        <Grid item xs={ 12 } sx={{ mt: 2}}>
-          <TextField
-            label="Contraseña"
-            type="password"
-            placeholder="Contraseña"
-            fullWidth
-            name= "password"
-            value={ password }
-            onChange={ onInputChange }
-          />
-        </Grid>
 
-        <Grid
-          container
-          display={ !!errorMessage ? '': 'none' }
-          sx={{ m1:1 }}
-        >
-          <Grid
-            item
-            xs={ 12 }
-          >
-            <Alert severity='error'>{ errorMessage }</Alert>
+            <Grid 
+              container
+              display={ !!errorMessage ? '': 'none' }
+              sx={{ mt: 1 }}>
+              <Grid 
+                  item 
+                  xs={ 12 }
+                >
+                <Alert severity='error'>{ errorMessage }</Alert>
+              </Grid>
+            </Grid>
+            
+            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <Button
+                  disabled={ isAuthenticating }
+                  type="submit" 
+                  variant='contained' 
+                  fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <Button
+                   disabled={ isAuthenticating }
+                   variant='contained' 
+                   fullWidth
+                   onClick={ onGoogleSignIn }>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+
+            <Grid container direction='row' justifyContent='end'>
+              <Link component={ RouterLink } color='inherit' to="/auth/register">
+                Crear una cuenta
+              </Link>
+            </Grid>
+
           </Grid>
-        </Grid>
-
-        <Grid container spacing={ 2 } sx={{ mb: 2, mt: 0.1 }}>
-          <Grid item xs={ 12 } sm={ 6 }>
-            <Button
-              disabled={ isAuthenticating }
-              type="submit"
-              variant="contained"
-              fullWidth
-            >
-              Login
-            </Button>
-          </Grid>
-
-          <Grid item xs={ 12 } sm={ 6 }>
-            <Button
-              disabled={ isAuthenticating }
-              variant="contained"
-              fullWidth
-              onClick={ onGoogleSignIn }
-            >
-              <Google />
-              <Typography sx={{ ml: 1 }}>Google</Typography>
-            </Button>
-          </Grid>
-
-        </Grid>
-
-        <Grid container direction='row' justifyContent='end'>
-          <Link component={ RouterLink } color="inherit" to="/auth/register">
-            Crear una cuenta
-          </Link>
-        </Grid>
-
-      </Grid>
 
 
-      </form>
+        </form>
+
     </AuthLayout>
-      
   )
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,52 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getEnvironments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// console.log( import.meta.env)
+// console.log(process.env);
+
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_DATABASEURL,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
+
 // Your web app's Firebase configuration
+// Dev/Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAQJOI0jxNjbWeA-6CLhqKHQbaThXQY68E",
+//   authDomain: "react-curso-21a27.firebaseapp.com",
+//   projectId: "react-curso-21a27",
+//   storageBucket: "react-curso-21a27.appspot.com",
+//   messagingSenderId: "963836410354",
+//   appId: "1:963836410354:web:ae31aa13689ac36b95136b"
+// };
+
+// Testing
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB9ZbvyDg0tRATaCOstQcgQ0uNw0DwLtrw",
+//   authDomain: "testing-8ae5a.firebaseapp.com",
+//   projectId: "testing-8ae5a",
+//   storageBucket: "testing-8ae5a.appspot.com",
+//   messagingSenderId: "924455406586",
+//   appId: "1:924455406586:web:a4e19f7dfa1f0f285eb243"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAQJOI0jxNjbWeA-6CLhqKHQbaThXQY68E",
-  authDomain: "react-curso-21a27.firebaseapp.com",
-  projectId: "react-curso-21a27",
-  storageBucket: "react-curso-21a27.appspot.com",
-  messagingSenderId: "963836410354",
-  appId: "1:963836410354:web:ae31aa13689ac36b95136b"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  databaseURL: VITE_DATABASEURL,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 // Initialize Firebase
