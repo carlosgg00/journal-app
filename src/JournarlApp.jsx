c16f9afa367be70d52e51680c4caf7a7a8cf4506
carlosgg00 <garciaariel@unah.hn>
2023-02-05 15:30:25 -0600

    recargar pagina arreglado 1.0

--- src/JournarlApp.jsx
+++ src/JournarlApp.jsx
@@ -1,13 +1,10 @@
-import { HashRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
 import { AppTheme } from "./theme"
 
 export const JournarlApp = () => {
   return (
     <AppTheme>
-      <HashRouter>
         <AppRouter />
-      </HashRouter>
     </ AppTheme>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { JournarlApp } from './JournarlApp'
 import { store } from './store'
 import './styles.css'
@@ -9,9 +9,9 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <BrowserRouter>
+      <HashRouter>
         <JournarlApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
